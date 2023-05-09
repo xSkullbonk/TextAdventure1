6925eba4168edcee6aa311a4ebb4a2773794f18d
rc7wx <janis.mayer92@gmail.com>
2023-05-09 22:46:07 +0200

    kleine nachbesserungen

--- functions.py
+++ functions.py
@@ -5,4 +5,5 @@ def slow_print(input_str):
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.05)
+    time.sleep(0.3)
     sys.stdout.write('\n')
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import sys, time, scenes
+import sys, time, scenes, classes
 
 # Langsames printen, damit es wie getippt wirkt
-def slow_print(input_str):
-    for c in input_str:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.05)
-    sys.stdout.write('\n')
+# def slow_print(input_str):
+#     for c in input_str:
+#         sys.stdout.write(c)
+#         sys.stdout.flush()
+#         time.sleep(0.05)
+#     sys.stdout.write('\n')
 
 
 # Klasse für Spielerobjekt
