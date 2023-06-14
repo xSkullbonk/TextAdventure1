2e866181d78686ded9a094d7d7dc8fe2abae15f6
rc7wx <janis.mayer92@gmail.com>
2023-06-14 13:48:15 +0200

    Struktur Änderung wegen Sprachen

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import sys, time, scenes, classes
+import scenes, classes
 
-# Langsames printen, damit es wie getippt wirkt
-# def slow_print(input_str):
-#     for c in input_str:
-#         sys.stdout.write(c)
-#         sys.stdout.flush()
-#         time.sleep(0.05)
-#     sys.stdout.write('\n')
-
-
-# Klasse für Spielerobjekt
-# class playerobject:
-#     def __init__(self, name):
-#         self.name = name
-#         self.inventory = []
-
-
-       
-    
+  
 
 # Start des Spieldurchlaufes
 scenes.intro()
